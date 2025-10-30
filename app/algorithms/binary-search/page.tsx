0823804404193f94771cb2d4d5c20ffbc9fe2739
NZ <ns@zav.me>
2025-10-30 13:53:04 +0300

    first commit

---
+++ app/algorithms/binary-search/page.tsx
@@ -0,0 +1,42 @@
+import React from "react";
+
+export default function Page() {
+
+    const binarySearch = (arr, item) => {
+        let lowIndex = 0;
+        let highIndex = arr.length - 1;
+        while (lowIndex <= highIndex) {
+
+            const middleIndex = Math.floor((lowIndex + highIndex) / 2);
+            const guess = arr[middleIndex];
+            if (guess === item) {
+                return middleIndex;
+            } if (guess > item) {
+
+                highIndex = middleIndex - 1;
+            } else {
+                lowIndex = middleIndex + 1;
+            }
+        }
+    }
+    const myList = [11, 33, 55, 77, 99];
+
+    return (
+        <div className='p-20'>
+            <h2>Сортировка выбором</h2>
+            <p>list = {myList.join(', ')}</p>
+            <p>
+                binarySearch(myList, 11)
+                <span className='text-blue-300'> // {binarySearch(myList, 11)}</span>
+            </p>
+            <p>
+                binarySearch(myList, 33)
+                <span className='text-blue-300'> // {binarySearch(myList, 33)}</span>
+            </p>
+            <p>
+                binarySearch(myList, 99)
+                <span className='text-blue-300'> // {binarySearch(myList, 99)}</span>
+            </p>
+        </div>
+    );
+}
